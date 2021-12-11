56953593cd3ae70096513370d17466f10f034ee4
joanribes <91140408+joanribes@users.noreply.github.com>
2021-12-11 14:32:41 +0100

    Rel, Sun & Planet

--- src/App.js
+++ src/App.js
@@ -9,18 +9,20 @@ import Planet from './models/planet.js';
 import Light from './models/light.js';
 import Sun from './models/sun.js';
 import { PerspectiveCamera } from 'three';
-function App() {
+import { Rel } from './functions/constants';
+function App({ x }) {
   const camera = new PerspectiveCamera(70, window.innerWidth/window.innerHeight, 0.1, 1000 );
-  camera.position.set(0,2,3); 
+  camera.position.set(0,2,3);
+  const rel = new Rel(x);
   return (
   <Suspense fallback={<Loader />}>
     <Canvas shadows shadowMap camera={camera}>
       <OrbitControls enablePan={false} enableZoom={false} position0={[5, 0, 0]}/>
       <MyStars />
       <ambientLight intensity= {0.5} />
-      <Sun position={[500, 0, 0]} size={200} base={"/sun.jpeg"}/>
+      <Sun position={[500, 0, 0]} size={rel.calc('sunSize')} base={"/sun.jpeg"}/>
       <Light brightness={2} color={"white"} position={[10, 0, 0]}/>
-      <Planet position={[0, 0, 0]} size={1} base={"/earth.jpeg"} orbit={500}/>
+      <Planet position={[0, 0, 0]} size={rel.calc('planetSize')} base={"/earth.jpeg"} orbit={500}/>
       <Satelite position={[1, 0, 0]} size={0.1} orbit={1.5}/>
     </Canvas>
   </Suspense>
---
+++ src/functions/constants.js
@@ -0,0 +1,36 @@
+const relData = { sunSize: 200, planetSize: 1 };
+
+class Rel {
+    static defaultData = relData;
+    static defaultErrval = 0;
+    data = Rel.defaultData;
+    errval = Rel.defaultErrval;
+    /**
+     * Rel Constructor
+     * @param {number} x Relationship base.
+     */
+    constructor(x) {
+        this.x = x;
+    }
+
+    /**
+     * Get the value of `name`.
+     * @param {("sunSize" | "planetSize")} name
+     * @param {boolean} useDefaultData Use the default values.
+     * @returns {number} name value.
+     */
+    get(name, useDefaultData) {
+        return (useDefaultData ? Rel.defaultDataData : this.data)[name] || this.errval;
+    }
+
+    /**
+     * Calculate the size of the specified `name`.
+     * @param {("sunSize" | "planetSize")} name Identifier.
+     * @param {boolean} useDefaultData Use the default values.
+     * @returns {number} `<this.x> * <name>` or `this.errval` if `name` doesn't exists.
+     */
+    calc(name, useDefaultData) {
+        return ((useDefaultData ? Rel.defaultDataData : this.data)[name] || this.errval) * this.x;
+    }
+}
+export { Rel };
