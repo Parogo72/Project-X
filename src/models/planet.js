0051e7bb51116c9df3bc220f9496011d3c41145a
Parogo_72 <76404961+Parogo72@users.noreply.github.com>
2021-12-16 17:18:19 +0100

    Update planet.js

--- src/models/planet.js
+++ src/models/planet.js
@@ -16,7 +16,7 @@ function Planet({ position, base, size, orbit }) {
           <sphereGeometry args={[size, 30, 30]} />
           <meshStandardMaterial/>
         </mesh>
-        
+        <Orbit xRadius={orbit} zRadius={orbit} position={[200,0,0]} />
       </>
     );
 }
