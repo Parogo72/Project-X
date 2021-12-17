727055b82bc7e123f37f29847cb3b7f74a198a00
Parogo_72 <76404961+Parogo72@users.noreply.github.com>
2021-12-17 08:21:49 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ import { Rel } from './functions/constants';
 function App({ x }) {
   const camera = new PerspectiveCamera(70, window.innerWidth/window.innerHeight, 0.1, 1000 );
   camera.position.set(0,2,3);
+  const texture = useLoader(TextureLoader, "/sun.jpeg");
+  texture.wrapS = RepeatWrapping;
   const rel = new Rel(x);
   return (
   <Suspense fallback={<Loader />}>
@@ -20,7 +22,7 @@ function App({ x }) {
       <OrbitControls enablePan={false} enableZoom={false} position0={[5, 0, 0]}/>
       <MyStars />
       <ambientLight intensity= {0.5} />
-      <Sun position={[500, 0, 0]} size={rel.calc('sunSize')} base={"/sun.jpeg"}/>
+      <Sun position={[500, 0, 0]} size={rel.calc('sunSize')} base={texture}/>
       <Light brightness={2} color={"white"} position={[10, 0, 0]}/>
       <Planet position={[0, 0, 0]} size={rel.calc('planetSize')} base={"/earth.jpeg"} orbit={500}/>
       <Satelite position={[1, 0, 0]} size={0.1} orbit={1.5}/>
